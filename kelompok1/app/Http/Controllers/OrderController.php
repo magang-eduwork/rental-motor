bdcd021079ade0ab11b77c3f8eda1ac9f56aad13
Fawwas Alghifari <fawwasalghifari20@gmail.com>
2026-07-02 20:43:56 +0700

    db order,product,order items, payment, order histories

---
+++ kelompok1/app/Http/Controllers/OrderController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\View\View;
+
+class OrderController extends Controller
+{
+    /**
+     * Menampilkan daftar pesanan.
+     *
+     * @return View
+     */
+    public function index(): View
+    {
+        $orders = Order::latest()->get();
+        
+        dd($orders); 
+
+        return view('order.index', compact('orders'));
+    }
+}
---
+++ kelompok1/database/migrations/2026_07_01_150944_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode_booking');
+            $table->string('nama_motor');
+            $table->date('tanggal_booking');
+            $table->string('status'); // Contoh status: Belum bayar, Sedang dibawa, dll
+            $table->decimal('harga', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
