ae057ac2a9084fcc9d9ad196c616d54517e1a613
altros.chiho <chihoiuwei@gmail.com>
2026-07-13 11:15:45 +0700

    update product seeder

--- kelompok1/database/seeders/ProductSeeder.php
+++ kelompok1/database/seeders/ProductSeeder.php
@@ -9,24 +9,25 @@ class ProductSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     * https://ibb.co.com/5xf0FhHv
      */
+
     public function run(): void
     {
         $motors = [
-            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9001 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
-            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9002 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
-            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9003 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
-            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9004 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
-            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9005 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
-            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9006 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
-            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9007 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
-            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9008 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
-            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9009 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
-            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9010 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
-            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9011 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
-            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9012 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
+            ['nama_motor' => 'Vega Force', 'tipe' => 'Motor Bebek', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9001 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
+            ['nama_motor' => 'Vega Force', 'tipe' => 'Motor Bebek', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9002 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
+            ['nama_motor' => 'Vega Force', 'tipe' => 'Motor Bebek', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9003 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
+            ['nama_motor' => 'Vega Force', 'tipe' => 'Motor Bebek', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9004 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
+            ['nama_motor' => 'Vega Force', 'tipe' => 'Motor Bebek', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9005 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
+            ['nama_motor' => 'Vega Force', 'tipe' => 'Motor Bebek', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9006 DE', 'image_url' => 'https://i.ibb.co.com/VYfhgqm4/image-44.png'],
+            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9007 DE', 'image_url' => 'https://i.ibb.co.com/FqjrV4TJ/image-15.png'],
+            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9008 DE', 'image_url' => 'https://i.ibb.co.com/FqjrV4TJ/image-15.png'],
+            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9009 DE', 'image_url' => 'https://i.ibb.co.com/FqjrV4TJ/image-15.png'],
+            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9010 DE', 'image_url' => 'https://i.ibb.co.com/FqjrV4TJ/image-15.png'],
+            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9011 DE', 'image_url' => 'https://i.ibb.co.com/FqjrV4TJ/image-15.png'],
+            ['nama_motor' => 'Vega Force', 'tipe' => 'Matic', 'harga_per_hari' => 75000, 'status' => 'tersedia', 'plat_nomor' => 'AB 9012 DE', 'image_url' => 'https://i.ibb.co.com/FqjrV4TJ/image-15.png'],
         ];
-
         foreach ($motors as $motor) {
             \App\Models\Product::create($motor);
         }
